b468928a2f5ebadfd0cc0e81793af102d854acb4
Bryan Haakman <bryanhaakman@gmail.com>
2016-01-12 11:13:45 +0100

    utils compatible with 1.9

--- djorm_pgfulltext/utils.py
+++ djorm_pgfulltext/utils.py
@@ -1,10 +1,9 @@
 import psycopg2
 
-from django.db import connection
+from django.db import connections
 from django.utils.text import force_text
-
 
 def adapt(text):
     a = psycopg2.extensions.adapt(force_text(text))
-    a.prepare(connection.connection)
+    a.prepare(connections['default'].cursor().connection)
     return a
